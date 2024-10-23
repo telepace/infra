04c6609cd9cb8fb7bdc6cb16c4b19f1da5311794
熊鑫伟 Xinwei Xiong <3293172751NSS@gmail.com>
2024-10-23 15:54:44 +0800

    Update source.sh
    Signed-off-by: 熊鑫伟 Xinwei Xiong <3293172751NSS@gmail.com>

--- compose/production/ubuntu/source.sh
+++ compose/production/ubuntu/source.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export OS=$(/openim/tools/get_os.sh)
-export ARCH=$(/openim/tools/get_arch.sh)
+export OS=$(/telepace/tools/get_os.sh)
+export ARCH=$(/telepace/tools/get_arch.sh)
 
 exec "$@"
